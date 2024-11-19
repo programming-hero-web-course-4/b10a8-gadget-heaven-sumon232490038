388e839456caabbe8da9dc5293f6be44782c0d8a
sumon232 <sumonmia4526@gmail.com>
2024-11-19 16:47:17 +0600

    Error page setup is done

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="">
+      <div className="mx-auto flex justify-center items-center flex-col my-auto h-[500px]">
+        <h1 className="text-[300px]	mb-0  font-black">404</h1>
+        <h1 className="text-7xl">Oh! Sorry this page is not found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./components/Root/Root";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     // children: [
     //   {
     //     path: "/",
