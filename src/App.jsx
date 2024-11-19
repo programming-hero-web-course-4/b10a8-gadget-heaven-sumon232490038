723f929d236b5f0528f50d72843e87ec2bb8b70b
sumon232 <sumonmia4526@gmail.com>
2024-11-19 08:40:11 +0600

    React router , tailwindcss and daisyUi install and setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">This My ph assingment 08</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-800">
+        Welcome to my Assignment
+      </h1>
+    </div>
+  );
+};
+
+export default Root;
