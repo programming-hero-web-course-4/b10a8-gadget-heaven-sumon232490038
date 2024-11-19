986fc60f2f863befb4cb41c906dc4437955b0a4f
sumon232 <sumonmia4526@gmail.com>
2024-11-19 15:28:35 +0600

    Alhamdulillah banner img design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import BannerImg from "../BannerImg/BannerImg";
 
 const Banner = () => {
   return (
-    <div className=" max-w-7xl mx-auto p-5 md:px-40 h-[400px]">
+    <div className=" max-w-7xl mx-auto p-5 md:px-40 h-[500px] ">
       <div className="  text-center pt-0 ">
         <div className="">
-          <h1 className="text-5xl font-bold">
+          <h1 className="md:text-5xl text-2xl font-bold">
             Upgrade Your Tech Accessorize with Gadget Heaven Accessories
           </h1>
           <p className="py-6">
@@ -13,11 +14,12 @@ const Banner = () => {
             next level. From smart devices to <br /> the coolest accessories, we
             have it all!.
           </p>
-          <button className="px-8 btn p-3 font-bold bg-white rounded-3xl text-[#9538e2]">
+          <button className="px-8 btn p-3 font-bold bg-white rounded-3xl mb-10 text-[#9538e2]">
             Shop Now
           </button>
         </div>
       </div>
+      <BannerImg></BannerImg>
     </div>
   );
 };
---
+++ src/components/BannerImg/BannerImg.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BannerImg = () => {
+  return (
+    <div className="p-5 rounded-2xl border-2 max-w-4xl mx-auto mt-50 ">
+      <div className="w-full h-full">
+        <img
+          className="max-h-96 rounded-2xl w-full "
+          src="https://i.ibb.co.com/jkhJvpX/banner.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BannerImg;
