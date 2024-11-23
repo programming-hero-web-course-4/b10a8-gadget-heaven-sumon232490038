f1c970a95d1c000fb2d6a0c2638b19b18652074a
sumon232 <sumonmia4526@gmail.com>
2024-11-23 17:48:49 +0600

    somthing is added

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -111,7 +111,7 @@ const DashBoard = () => {
       <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
+          <p className="py-4 m-0">
             Press ESC key or click the button below to close
           </p>
           <div className="modal-action">
